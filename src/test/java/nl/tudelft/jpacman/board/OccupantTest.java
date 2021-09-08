5ea6198291b685b8fcd6ca9668db60482f869b66
nont4e <natchanon.thn@gmail.com>
2021-09-08 14:28:14 +0700

    OccupantList

--- src/test/java/nl/tudelft/jpacman/board/OccupantTest.java
+++ src/test/java/nl/tudelft/jpacman/board/OccupantTest.java
@@ -31,8 +31,7 @@ class OccupantTest {
      */
     @Test
     void noStartSquare() {
-        // Remove the following placeholder:
-        assertThat(unit).isNotNull();
+        assertThat(unit.hasSquare()).isEqualTo(false);
     }
 
     /**
@@ -41,8 +40,14 @@ class OccupantTest {
      */
     @Test
     void testOccupy() {
-        // Remove the following placeholder:
-        assertThat(unit).isNotNull();
+        //Before occupied
+        assertThat(unit.hasSquare()).isEqualTo(false);
+        //After occupied
+        Square square = new BasicSquare();
+        unit.occupy(square);
+        assertThat(unit.hasSquare()).isEqualTo(true);
+        assertThat(unit.getSquare()).isEqualTo(square);
+        assertThat(square.getOccupants()).contains(unit);
     }
 
     /**
@@ -51,7 +56,14 @@ class OccupantTest {
      */
     @Test
     void testReoccupy() {
-        // Remove the following placeholder:
-        assertThat(unit).isNotNull();
+        Square square1 = new BasicSquare();
+        Square square2 = new BasicSquare();
+
+        unit.occupy(square1);
+        unit.occupy(square2);
+        assertThat(unit.hasSquare()).isEqualTo(true);
+        assertThat(square1.getOccupants()).isEmpty();
+        assertThat(unit.getSquare()).isEqualTo(square2);
+        assertThat(square2.getOccupants()).contains(unit);
     }
 }
